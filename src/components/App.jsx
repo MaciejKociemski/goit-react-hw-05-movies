52287277f9418931f13abddb6631d4c22c8fca31
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-06-16 11:17:51 +0200

    fix [bug] with render SharedLayout
    not working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { BrowserRouter as Router,Routes, Route, Navigate
+} from 'react-router-dom';
+import { lazy } from 'react';
+import { Toaster } from 'react-hot-toast';
+// import { Loader } from './Loader/Loader';
+//add Suspense po lazy 
+
+const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+
 export const App = () => {
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template images homework 05
-    </div>
+    <>
+      <Router>
+        {/* <Suspense fallback={<div>Loading...please wait</div>}> */}
+          <SharedLayout>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            {/* <Route path="movies/:movieId" element={<MovieDetails />}> */}
+              {/* <Route path="cast" element={<Cast />} /> */}
+              {/* <Route path="reviews" element={<Reviews />} /> */}
+            {/* </Route> */}
+            {/* <Route path="*" element={<Navigate to="/" />} /> */}
+          </Routes>
+          </SharedLayout>
+        {/* </Suspense> */}
+        {/* <Toaster /> */}
+      </Router>
+    </>
   );
 };
+
